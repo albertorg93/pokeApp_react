a9edb6145ef753b166af3dd8c25c926586ce6273
albertorg93 <futberto1993@gmail.com>
2022-05-01 11:06:20 +0200

    first commit

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import ProductList from './ProductList'
+import Chefs from './Chefs'
+import About from './About'
+import Contact from './Contact'
+import Delivery from './Devivery'
+import { Route,Routes } from 'react-router-dom'
+import Topics from './Topics'
+
+export class Main extends Component {
+  render() {
+    return (
+      <main>
+          <h1>Futura web - Exploding Cakes!!</h1>
+          <p>Esto es el main</p>
+          <Routes>
+          <Route element={<ProductList/>} path='/'/>
+          <Route element={<Chefs/>} path='/listachefs'/>
+          <Route element={<About/>} path='/about'/>
+          <Route element={<Delivery/>} path='/delivery'/>
+          <Route element={<Topics/>} path='/topics'/>
+          <Route element={<Contact/>} path='/contact'/>
+
+          </Routes>
+          
+      </main>
+    )
+  }
+}
+
+export default Main
