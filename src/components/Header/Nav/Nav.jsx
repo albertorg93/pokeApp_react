7ee270b356388fee707b91963aa0fbe9f355d843
albertorg93 <futberto1993@gmail.com>
2022-05-02 12:29:26 +0200

    fixing last changes

--- src/components/Header/Nav/Nav.jsx
+++ src/components/Header/Nav/Nav.jsx
@@ -7,12 +7,7 @@ import { Link } from "react-router-dom";
 class Nav extends Component {
   render() {
     return <div className="nav">
-      <Link to="/" className="nav__element">Home</Link>
-      <Link to="/listachefs" className="nav__element">Lista Chefs</Link>
-      <Link to="/delivery" className="nav__element">Delivery</Link>
-      <Link to="/topics" className="nav__element">Topics</Link>
-      <Link to="/contact" className="nav__element">Contact</Link>
-      
+      <Link to="/card" className="nav__element">Home</Link>
     </div>;
   }
 }
